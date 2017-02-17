6a592225e287ab2e08d3beb3bbf4083577990a36
bumbacoin <bumbacoin@gmail.com>
2017-02-17 22:25:05 +0800

    Update version.h

--- src/version.h
+++ src/version.h
@@ -16,8 +16,8 @@
 // stronghands version - intended for display purpose only
 #define PEERUNITY_VERSION_MAJOR       0
 #define PEERUNITY_VERSION_MINOR       1
-#define PEERUNITY_VERSION_REVISION    3
-#define PEERUNITY_VERSION_BUILD       0
+#define PEERUNITY_VERSION_REVISION    4
+#define PEERUNITY_VERSION_BUILD       1
 
 static const int PEERUNITY_VERSION =
                            1000000 * PEERUNITY_VERSION_MAJOR
